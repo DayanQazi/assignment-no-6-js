c264170ff90339a23b9269cf5674e68e9c8bbe75
Dayan Qazi <dayanqazi622@gmail.com>
2024-03-05 02:56:42 +0500

    Work Done

---
+++ gitone/app.js
@@ -0,0 +1,47 @@
+// let num = 5;
+// for(let i=1; i<=8; i++){
+//     num *= i;
+// } 
+// console.log(num);
+
+
+
+
+// let score = 10;
+// for(let i=6; i<=12; i++){
+//     score += i;
+// }  
+// console.log(score);
+
+
+
+// for (let i=0; i<=100; i++){
+//     if(i%2 ==1){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i=1; i<=6; i++){
+//     let star="";
+//     for(let j=4; j<=i; j++){
+//         star = star + "*";
+//     }  console.log(star); 
+// }
+
+
+
+
+// let city = prompt("Enter Your City");
+// let cities=["Karachi","Lahore","Quetta","Islamabad"];
+// let found=false;
+// for(let i=0; i<cities.length; i++){
+//     if (city===cities[i]){
+//         console.log(city,"City found");
+//         found=true;
+//         break;
+//     }
+// }
+// if(!found){
+//     console.log(city, "City Not Found");
+// }
